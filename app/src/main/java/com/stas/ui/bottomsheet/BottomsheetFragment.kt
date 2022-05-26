2a6d4614bfe8653e79c00ab27befbf6bb441683b
Stahma <stasstahma@gmail.com>
2022-05-26 16:34:27 +0300

    bottom sheet content fit

--- app/src/main/java/com/stas/ui/bottomsheet/BottomsheetFragment.kt
+++ app/src/main/java/com/stas/ui/bottomsheet/BottomsheetFragment.kt
@@ -3,11 +3,14 @@ package com.stas.ui.bottomsheet
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.stas.appfortest.DataHolder
 import com.stas.appfortest.databinding.FragmentBottomsheetBinding
 import com.stas.ui.base.BaseFragment
 
 class BottomsheetFragment : BaseFragment<FragmentBottomsheetBinding>() {
+  private lateinit var bottomsheetBehavior: BottomSheetBehavior<ConstraintLayout>
 
   override fun provideViewBinding(
     inflater: LayoutInflater,
@@ -15,7 +18,11 @@ class BottomsheetFragment : BaseFragment<FragmentBottomsheetBinding>() {
   ): FragmentBottomsheetBinding = FragmentBottomsheetBinding.inflate(inflater, container, false)
 
   override fun prepareUi(savedInstanceState: Bundle?) {
-    super.prepareUi(savedInstanceState)
+    bottomsheetBehavior = BottomSheetBehavior.from(binding.sheetContainer)
     binding.sheetContent.text = DataHolder.lorem
+
+    binding.sheetExpand.setOnClickListener {
+      bottomsheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+    }
   }
 }
